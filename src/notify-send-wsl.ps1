c6876e6183f983b3073398789fc3ea0752be130e
Bruce Markham <219281+brucificus@users.noreply.github.com>
2024-12-18 12:34:27 -0500

    🐛 Defines Absent Global Variable

--- src/notify-send-wsl.ps1
+++ src/notify-send-wsl.ps1
@@ -4,6 +4,7 @@ Set-StrictMode -Version Latest
 #Requires -Modules @{ ModuleName = "poshy-lucidity"; RequiredVersion = "0.4.1" }
 
 
+$IsWSL = (Get-Variable -Name IsWSL -ValueOnly -ErrorAction SilentlyContinue) -or (Test-Path Env:\WSL_DISTRO_NAME -ErrorAction SilentlyContinue)
 [string] $wslnotifysend_bin = Search-CommandPath wsl-notify-send.exe
 if ($IsWSL -and $wslnotifysend_bin) {
     function notify-send-wsl {
